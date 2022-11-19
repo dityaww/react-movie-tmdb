ca3e621e156dc68108b1c6f8173b8d05f3f81fd6
dityaww <111202012838@mhs.dinus.ac.id>
2022-11-19 16:20:40 +0700

    still on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <>
       <div className="bg-neutral-800">
-        <div className="container mx-auto max-w-7xl ml-5 lg:ml-0">
+        <div className="container mx-auto max-w-7xl">
           <p className="text-neutral-300 font-extrabold text-2xl py-5">
             Movies.
           </p>
